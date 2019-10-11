f13391608de5cba0a63a2461c5687ef372f5e09c
kiyani <bamba.kiyani@gmail.com>
2019-10-11 04:15:28 -0400

    Built the reducer function

--- smurfs/src/reducers/reducers.js
+++ smurfs/src/reducers/reducers.js
@@ -1,3 +1,4 @@
+import * as actionType from "../actions/actions.js";
 
 // Design slice of state
 
@@ -9,3 +10,44 @@
     deletingSmurf: false,
     error: null
   };
+
+  export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+      case actionType.FETCH:
+        return {
+          ...state,
+          fetchingSmurfs:true
+        }
+        case actionType.FETCH_SUCCESS:
+          return {
+            ...state,
+            fetchingSmurfs: false,
+            smurfs: action.payload
+          }
+        case actionType.FETCH_FAILED:
+          return {
+            ...state,
+            fetchingSmurfs:false,
+            error: action.payload
+          }
+        case actionType.ADD_SMURF:
+          return {
+            ...state,
+            addingSmurf: true,
+          }
+        case actionType.ADD_SUCCESS : 
+          return {
+            ...state,
+            addingSmurf: false,
+            smurfs: action.payload
+          }
+        case actionType.ADD_FAILED: 
+          return {
+            ...state,
+            addingSmurf: false,
+            error: action.payload
+          }
+      default:
+        return state
+    }
+  }
