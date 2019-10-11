38d8644172da0cb49eae41e3c0bfea4703d70bea
kiyani <bamba.kiyani@gmail.com>
2019-10-11 04:24:35 -0400

    Connected our components

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from 'react-redux'; 
+import { fetchSmurfs } from "../actions/actions.js";
+
+
+// connect our components
+
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchSmurfs()
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
+        <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
       </div>
@@ -13,4 +24,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, { fetchSmurfs })(App) ;
